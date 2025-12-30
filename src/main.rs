eb1c7a31eef9eea6f876c0559860ce5fa3734fd1
Setaareh Nikbin <eris.elea@gmail.com>
2025-12-30 04:16:25 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use std::collections::HashMap;
+
+fn main() {
+    println!("{}", mod_regex(7, 2, 0));
+}
+
+#[wasm_bindgen]
+pub fn mod_regex(divisor : usize, base : usize, remainder: usize) -> String {
+    let base_encoding: Vec<String> = ('0'..='9')
+                                    .chain('A'..='F')
+                                    .map(|x| x.to_string())
+                                    .collect();
+    let mut dfa: Vec<HashMap::<String, usize>> = (0..divisor).map(|_| HashMap::<String, usize>::new()).collect();
+    // invariant: a State's index in the vector corresponds to its remainder
+    // later on, "removing" a state just corresponds to rearranging edges so none reach the given state
+
+    for (state, node) in dfa.iter_mut().enumerate() {
+        for (next_digit, encoding) in base_encoding.iter().enumerate().take(base) {
+            let r = (base * state  + next_digit) % divisor;
+            node.insert(encoding.clone(), r);
+        }
+    }
+
+    let order : Vec<usize>;
+    if remainder == 0{
+        order = (remainder..divisor).chain(0..remainder).rev().collect::<Vec<usize>>();
+    } else {
+        order = (remainder + 1..divisor).chain(1..remainder).rev().chain([remainder, 0]).collect::<Vec<usize>>();
+    }
+
+    for (i, &state) in order.iter().enumerate() { // we proceed in "backwards" looping order down to the accepting state
+        // collapse parallel edges
+        for node in dfa.iter_mut() {
+            let mut reduced: HashMap::<usize, Vec<String>> = HashMap::new(); //this is backwards
+            for (key, value) in &mut *node {
+                if reduced.keys().any(|x| x == value){
+                    reduced.get_mut(value).unwrap().push(key.clone());
+                } else {
+                    reduced.insert(*value, vec![key.clone()]);
+                }
+            }
+            node.clear();
+            for (key, value) in reduced {
+                if value.len() == 1 {
+                    node.insert(value[0].clone(), key);
+                } else{
+                    node.insert("(".to_string() + &value.join("|") + ")", key);
+                }
+            }
+        }
+
+        if state == remainder {
+            continue;
+        }
+
+        // TODO: optimize regexes
+        // TODO: add remainder != 0 support
+        
+        let mut recursive_edges: Vec<String> = Vec::new(); //this section accounts for any edges leading from the node to itself
+        for (key, &value) in dfa.get(state).unwrap() {
+            if value == state {
+                recursive_edges.push(key.clone());
+            }
+        } // technically somewhat redundant with the above?
+        
+        let mut recursive_string = String::new();
+        if ! recursive_edges.is_empty() {
+            recursive_string = "(".to_string() + &recursive_edges.join("|") + ")*";
+        }
+
+        for &other_state in order[i+1..].iter() {  // replace edges leading to the state to be removed
+            let prefixes = dfa.get(other_state)
+                            .unwrap()
+                            .iter()
+                            .filter(|(_, value)| **value == state)
+                            .map(|(key, _)| key.clone())
+                            .collect::<Vec<String>>();
+
+            if ! prefixes.is_empty() {
+                for k in &prefixes {
+                    dfa.get_mut(other_state).unwrap().remove(k); // prune the edges to be replaced, now that their keys are saved
+                }
+                for (key, value) in dfa.get(state).unwrap().clone() {
+                    if value != state {
+                        for prefix in &prefixes {
+                            let new_edge = prefix.clone() + &recursive_string + &key;
+                            dfa.get_mut(other_state).unwrap().insert(new_edge, value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if remainder != 0 {
+        let accepting_state = dfa.remove(remainder);
+        let starting_state = dfa.remove(0);
+        let mut accepting_recursion = String::new();
+        let mut starting_recursion = String::new();
+        let mut start_to_accept = String::new();
+        let mut accept_to_start = String::new();
+
+        for (key, value) in accepting_state {
+            if value == remainder {
+                accepting_recursion = key.clone();
+            } else {
+                accept_to_start = key.clone();
+            }
+        }
+
+        for (key, value) in starting_state {
+            if value == remainder {
+                start_to_accept = key.clone();
+            } else {
+                starting_recursion = key.clone();
+            }
+        }
+
+        return format!("({starting_recursion}|{start_to_accept}({accepting_recursion})*{accept_to_start})*{start_to_accept}({accepting_recursion})*"); 
+    }
+    "^(".to_string()
+     + &dfa.get(remainder)
+        .unwrap()
+        .keys()
+        .cloned()
+        .collect::<Vec<String>>()
+        .join("|") + ")*$"
+}
